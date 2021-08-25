48bc5f33e23cb2e00eea0a3fc45228eb6de4f38a
Alessandro Branchetti <brk.alessandro@gmail.com>
2021-08-25 21:43:39 +0200

    Dependency Injection

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"github.com/brnktt/todo-app"
 	"github.com/brnktt/todo-app/pkg/handler"
+	"github.com/brnktt/todo-app/pkg/repository"
+	"github.com/brnktt/todo-app/pkg/service"
 	"log"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
