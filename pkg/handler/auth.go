25a7d5e43ce4ba2892623e281177cdda4e14f7ce
Alessandro Branchetti <brk.alessandro@gmail.com>
2021-08-29 22:55:41 +0200

    User authorization

--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -1,9 +1,28 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/brnktt/todo-app"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func (h *Handler) singUp(c *gin.Context) {
+	var input todo.User
 
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, "invalid input body")
+		return
+	}
+
+	id, err := h.services.Authorization.CreateUser(input)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"id": id,
+	})
 }
 
 func (h *Handler) singIn(c *gin.Context) {
--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -1,9 +1,12 @@
 package repository
 
-import "github.com/jmoiron/sqlx"
+import (
+	"github.com/brnktt/todo-app"
+	"github.com/jmoiron/sqlx"
+)
 
 type Authorization interface {
-
+	CreateUser(user todo.User) (int, error)
 }
 
 type TodoList interface {
@@ -21,5 +24,7 @@ type Repository struct {
 }
 
 func NewRepository(db *sqlx.DB) *Repository {
-	return &Repository{}
+	return &Repository{
+		Authorization: NewAuthPostgres(db),
+	}
 }
